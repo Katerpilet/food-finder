6d3594daf2320e3ef8419e31a9cc7a7c09b63b2e
Blake Gross <Katerpilet@gmail.com>
2014-01-30 17:45:59 -0500

    Added foursquare API demo

---
+++ engineering/demos/foursquareAPIDemo/foursquareAPIDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  foursquareAPIDemo
+//
+//  Created by Student on 1/30/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+- (void) foursquareConnectionTest;
+@end
